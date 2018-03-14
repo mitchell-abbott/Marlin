5173a0634b282e3a235d002b8e58142493f09f48
Mitchell Abbott <mitchabbott7@gmail.com>
2018-03-13 22:08:29 -0500

    Fix to failed compile

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -156,7 +156,7 @@
   #include "pins_MEGATRONICS_3.h"     // ATmega2560
 #elif MB(RAMBO)
   #include "pins_RAMBO.h"             // ATmega2560
-#elif MB(BOARD_EINSYRAMBO)
+#elif MB(EINSYRAMBO)
   #include "pins_EINSYRAMBO.h"        // ATmega2560
 #elif MB(MINIRAMBO) || MB(MINIRAMBO_10A)
   #include "pins_MINIRAMBO.h"         // ATmega2560
